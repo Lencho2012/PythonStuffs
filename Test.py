a8cc5c24029fe15324d28e1800e4525c0533e6da
Angel Martinez <angelm2012@gmail.com>
2016-01-04 11:17:39 -0600

    Stuffs

---
+++ Test.py
@@ -0,0 +1,20 @@
+
+
+squares = [1, 4, 9, 16]
+sum = 0
+for num in squares:
+	sum += num
+print sum ##30?
+
+list = ['larry', 'curly', 'moe']
+if 'curly' in list:
+	print 'yay'
+
+for i in range(5):
+	print 'dodo'
+
+a = ['red', 'green', 'blue']
+i = 0
+while i < len(a):
+	print a[i]
+	i = i + 1
