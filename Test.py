e38a860c484491bcfead37acf07912f066c02cd3
Angel Martinez <angelm2012@gmail.com>
2016-01-04 11:54:21 -0600

    More stuffs

--- Test.py
+++ Test.py
@@ -18,3 +18,34 @@ i = 0
 while i < len(a):
 	print a[i]
 	i = i + 1
+	
+print list[:]
+
+list = ['a', 'b', 'c', 'd']
+list[0:2] = 'z'
+print list
+
+
+a = [5, 1, 4, 3]
+print sorted(a)
+print a
+
+strs = ['aa', 'BB', 'zz', 'CC']
+print sorted(strs, reverse=True)
+print sorted(strs)
+
+(x, y, z) = (42, 13, 'hike')
+print z 
+
+nums = [1, 2, 3, 4]
+sqrs = [n * n for n in nums]
+
+print sqrs
+
+str = ['hello', 'and', 'goodbye']
+shout = [s.upper() + '!!!' for s in str]
+print shout
+
+nums = [2, 8, 1, 5]
+small = [n for n in nums if n <= 2]
+print small
